31a654d2a8a77c526e394f4bd7d571aceb393ad7
yvonne <jjovovic194@icloud.com>
2018-09-07 12:22:46 +0200

    mapa prosirena na ceo ekran ona mala ikonica

--- Restorani preko koda/AllRestarantsController.swift
+++ Restorani preko koda/AllRestarantsController.swift
@@ -10,17 +10,17 @@ import UIKit
 import MapKit
 
 
-class AllRestarantsController: UIViewController {
-    
+class AllRestarantsController: UIViewController , MKMapViewDelegate  {
+    let allMapView : MKMapView = {
+    let mv = MKMapView()
+    return mv
+}()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        
-    }
-    
-    
+        view.addSubview(allMapView)
+        allMapView.anchor(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right:view.rightAnchor , topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
 }
 
 
-
-
+}
